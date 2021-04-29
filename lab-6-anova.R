b7e71f47d7d6fb4b5663e5429775fb64bd4b429d
Marin Eeg <ad7836fs@go.minnstate.edu>
2021-04-28 21:05:58 -0500

    Final commit

--- lab-6-anova.R
+++ lab-6-anova.R
@@ -9,13 +9,26 @@ library(tidyverse)
 crabs <- read_csv("chap15q27FiddlerCrabFans.csv")
 crabs
 
+
+
+
+
 # QUESTION D --------------------------------------------------------------
 
 # graph the distribution of body temperatures for each crab type
 
+ggplot(crabs, aes(x=crabType, y = bodyTemperature)) +
+  geom_point(size = 2, color = "red") +
+  geom_smooth (method = lm)
 
 
 # QUESTION E --------------------------------------------------------------
 
 # ANOVA
+
+aov_crabs <-
+  aov(bodyTemperature ~ crabType, data = crabs)
+aov_crabs
+
+summary(aov_crabs)
 
